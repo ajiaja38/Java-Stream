bb5051c856cab71758e6292c832413b12cd35018
masundareza123 <ngekngek328@gmail.com>
2023-12-26 23:30:05 +0700

    update minor

---
+++ src/Looping.java
@@ -0,0 +1,34 @@
+public class Looping {
+    public static void main(String[] args) {
+        final int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        final int limit = 16;
+        for (int i = 1; i <= limit; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        int result = 0;
+        for (int i = 1; i <= 5; i++) {
+            result += i;
+        }
+        System.out.println(result);
+
+    }
+}
